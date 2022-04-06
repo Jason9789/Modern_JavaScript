c8e3ddc2f4a5d004ecb9bf7012ff4210ac12a8b5
Jason9789 <lifebook0809@gmail.com>
2022-04-06 11:11:08 +0900

    study: type conversion

---
+++ tutorial/Part01/JavaScript_Fundamentals/interaction.js
@@ -0,0 +1,9 @@
+alert('hello');
+
+// result = prompt(title, [default])
+
+let age = prompt('나이를 입력해주세요', 100);
+alert(`당신의 나이는 ${age}살 입니다.`);
+
+let isBoss = confirm('당신이 주인인가요?');
+alert(isBoss);
---
+++ tutorial/Part01/JavaScript_Fundamentals/typeConversion.js
@@ -0,0 +1,16 @@
+// 문자형으로 변환
+
+let value = true;
+console.log(typeof value);
+
+value = String(value);
+console.log(typeof value);
+
+// 숫자형으로 변환
+console.log('6' / '2');
+
+let str = '123';
+console.log(typeof str);
+
+let num = Number(str);
+console.log(typeof num);
