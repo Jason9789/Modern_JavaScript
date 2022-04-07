62568018666429a0463dcf25bca1630d63959c14
Jason9789 <lifebook0809@gmail.com>
2022-04-07 15:01:29 +0900

    study: loops, switch

---
+++ tutorial/Part01/JavaScript_Fundamentals/loops.js
@@ -0,0 +1,17 @@
+let i = 0;
+while (i < 3) {
+  console.log(i);
+  i++;
+}
+
+i = 3;
+while (i) {
+  console.log(i);
+  i--;
+}
+
+console.log('\nfor loop');
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
