79157274bf456becd4a4f6c6b4a0be45bf4b95da
Jason9789 <lifebook0809@gmail.com>
2022-04-08 16:41:55 +0900

    setup: eslint, prettier

--- tutorial/Part01/JavaScript_Fundamentals/arrowFunction.js
+++ tutorial/Part01/JavaScript_Fundamentals/arrowFunction.js
@@ -3,37 +3,37 @@ let sum = function(a, b) {
   return a + b;
 }
 */
-let sum = (a, b) => a + b;
-alert(sum(1, 2));
+let sum = (a, b) => a + b
+alert(sum(1, 2))
 
-let double = (n) => n * 2;
-alert(double(2));
+let double = (n) => n * 2
+alert(double(2))
 
-let sayHi = () => alert('안녕하세요!');
-sayHi();
+let sayHi = () => alert('안녕하세요!')
+sayHi()
 
-let age = prompt('나이를 알려주세요.', 18);
-let welcome = age < 18 ? () => alert('안녕') : () => alert('안녕하세요');
-welcome();
+let age = prompt('나이를 알려주세요.', 18)
+let welcome = age < 18 ? () => alert('안녕') : () => alert('안녕하세요')
+welcome()
 
 let sum2 = (a, b) => {
-  let result = a + b;
-  return result;
-};
-alert(sum2(4, 4));
+  let result = a + b
+  return result
+}
+alert(sum2(4, 4))
 
 // 과제
 let ask = (question, yes, no) => {
-  if (confirm(question)) yes();
-  else no();
-};
+  if (confirm(question)) yes()
+  else no()
+}
 
 ask(
   '동의하십니까?',
   () => {
-    alert('동의하셨습니다.');
+    alert('동의하셨습니다.')
   },
   () => {
-    alert('취소 버튼을 누르셨습니다.');
+    alert('취소 버튼을 누르셨습니다.')
   }
-);
+)
